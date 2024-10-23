ef03655fcff5aeccfefd0071f2a948a7a05daaf8
niazananev698 <niazananev698@gmail.com>
2024-10-23 22:19:23 +0500

    fix[TelegramSmsBridge.API.csproj]: remove useless files and add new method to handling webhook from telegram bot

--- TelegramSmsBridge.API/Controllers/TelegramWebHookController.cs
+++ TelegramSmsBridge.API/Controllers/TelegramWebHookController.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TelegramSmsBridge.API.Controllers;
 
@@ -9,5 +13,39 @@ public class TelegramWebHookController : BaseApiController
     public TelegramWebHookController(ITelegramBotClient botClient)
     {
         _botClient = botClient;
+    }
+
+    [HttpPost("webhook")]
+    public async Task<IActionResult> Webhook([FromBody] Update update)
+    {
+        // TODO: set webhook secret_token while setting it
+        Request.Headers.TryGetValue("X-Telegram-Bot-Api-Secret-Token", out StringValues signature);
+        if (string.IsNullOrEmpty(signature))
+        {
+            return Unauthorized("Unauthorized Request");
+        }
+
+        if (update.Type == UpdateType.Message)
+        {
+            var message = update.Message;
+
+            if (message.Text != null)
+            {
+                switch (message.Text.Split(' ')[0])
+                {
+                    case "/start":
+                        await _botClient.SendTextMessageAsync(message.Chat.Id, "Welcome to the bot!");
+                        break;
+                    case "/help":
+                        await _botClient.SendTextMessageAsync(message.Chat.Id, "How can I help you?");
+                        break;
+                    default:
+                        await _botClient.SendTextMessageAsync(message.Chat.Id, "Unknown command.");
+                        break;
+                }
+            }
+        }
+
+        return Ok();
     }
 }
