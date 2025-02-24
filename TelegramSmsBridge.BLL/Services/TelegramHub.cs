35c260be1fa0b2b8a065e2672b6f812534b91b07
niazananev698 <niazananev698@gmail.com>
2025-02-24 12:35:59 +0500

    fix[TelegramHub.cs]: remove useless checking existing of user

--- TelegramSmsBridge.BLL/Services/TelegramHub.cs
+++ TelegramSmsBridge.BLL/Services/TelegramHub.cs
@@ -10,11 +10,6 @@ public class TelegramHub : Hub
     // Отправляет сообщение определённому зарегестрированному пользователю
     public async Task SendMessageAsync(string user, SmsMessage message)
     {
-        if (UserCollection.Instance.FirstOrDefaultUser(u => u.TelegramUserName == user) == null)
-        {
-            throw new InvalidOperationException($"Unauthorized user {user}");
-        }
-
         await Clients.User(user).SendAsync("ReceiveMessage", message);
     }
 }
